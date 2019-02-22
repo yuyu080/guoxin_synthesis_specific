18a4f939043a3e0fd0b9f0522064672d84861826
Zhao <zhaoyunfeng@bbdservice.com>
2019-02-22 18:14:05 +0800

    修改log生成方式

--- src/utils/logUtil.py
+++ src/utils/logUtil.py
@@ -8,8 +8,8 @@ LOG_DIR = BASE_DIR + '/logs/'
 
 def create_logger(logger_name, file_name):
     fh = RotatingFileHandler(LOG_DIR+file_name,
-                                              encoding='utf-8', mode='a+',
-                                              maxBytes=1024 * 1024, backupCount=40)
+                             encoding='utf-8',
+                             maxBytes=1024 * 1024, backupCount=40)
     logger = logging.getLogger(logger_name) #获得一个logger对象，默认是root
     logger.setLevel(logging.INFO)  #设置最低等级debug
     fm = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
